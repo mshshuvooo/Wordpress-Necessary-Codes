a00fb342dfcbcd3003132d056d3000de8d441f45
Shahadat Shuvo <shuvombm02@gmail.com>
2019-04-16 17:02:33 +0600

    Update wp-logo.php

--- wp-logo.php
+++ wp-logo.php
@@ -1,12 +1,10 @@
 <div class="logo">
-    <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-        <?php 
-        if(has_custom_logo()): 
-            the_custom_logo();
-        else: ?>
-            <h3><?php bloginfo( 'name' ); ?></h3> 
-        <?php 
-        endif;
-        ?>
-    </a>
+    <?php 
+    if(has_custom_logo()): 
+        the_custom_logo();
+    else: ?>
+        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><h3><?php bloginfo( 'name' ); ?></h3></a>
+    <?php 
+    endif;
+    ?>
 </div>
