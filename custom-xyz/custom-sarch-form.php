a39a78ede8601f6d40cbd31af6544565bc915d9f
Shahadat Shuvo <shuvombm02@gmail.com>
2022-03-06 09:17:54 +0600

    Delete custom-sarch-form.php

--- custom-xyz/custom-sarch-form.php
+++
@@ -1,6 +0,0 @@
-<form class="searchForm" action="<?php echo esc_url( home_url( '/' ) ); ?>" method="GET" autocomplete="off">
-    <input class="searchForm__input" type="search" name="s" value="" placeholder="Looking for Something?" autocomplete="off" />
-    <button class="searchForm__submit" type="submit">
-        <i class="fa fa-search" aria-hidden="true"></i>
-    </button>
-</form>
