0e086c49443b1c08118867de19805eda8c70d685
Shahadat Shuvo <shuvombm02@gmail.com>
2022-03-06 06:42:34 +0600

    Update custom-sarch-form.php

--- custom-xyz/custom-sarch-form.php
+++ custom-xyz/custom-sarch-form.php
@@ -1,4 +1,4 @@
-<form class="searchForm" action="" method="GET" autocomplete="off">
+<form class="searchForm" action="<?php echo esc_url( home_url( '/' ) ); ?>" method="GET" autocomplete="off">
     <input class="searchForm__input" type="search" name="s" value="" placeholder="Looking for Something?" autocomplete="off" />
     <button class="searchForm__submit" type="submit">
         <i class="fa fa-search" aria-hidden="true"></i>
