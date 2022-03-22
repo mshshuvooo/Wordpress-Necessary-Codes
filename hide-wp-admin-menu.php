04c44a3b6c4b71cf4c7b1ea56ea4d73cca751078
Shahadat Shuvo <shuvombm02@gmail.com>
2022-03-22 14:47:44 +0600

    Update hide-wp-admin-menu.php

--- hide-wp-admin-menu.php
+++ hide-wp-admin-menu.php
@@ -1,3 +1,5 @@
+<?php
+
 function msh_hide_admin_menus() {
 	remove_menu_page('woocommerce'); // Hide Woococommerce admin menu
 	remove_menu_page('user-registration'); //Add page slug to hide any admin page
